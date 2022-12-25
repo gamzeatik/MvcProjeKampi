091437a231c8c30e5b9681ae46c857c313d6ccd8
gamzeatik <gamzeatik33@hotmail.com>
2022-12-25 17:52:18 +0300

    son edit yetkilendirme
    done :)

---
+++ MvcProjeKampi/MvcProjeKampi/MvcProjeKampi/Controllers/AuthorizationController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        // GET: Authorization
+        AdminManager am = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminValues = am.GetList();
+            return View(adminValues);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin admin)
+        {
+            am.AdminAdd(admin);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var admin = am.GetByID(id);
+            return View(admin);
+        }
+        [HttpPost]
+        public ActionResult Edit(Admin p)
+        {
+            am.AdminUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
